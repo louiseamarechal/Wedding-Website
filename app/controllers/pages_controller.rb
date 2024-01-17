ce2031386fca775d717b12cc144f53a494d66f92
louiseameliemarechal <louiseamelie2@gmail.com>
2024-01-17 23:04:06 +0100

    adding NavBar and Project Routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def place
+  end
+  def housing
+  end
+  def program
+  end
+  def pictures
+  end
+  def rsvp
+  end
+  def presentsList
+  end
+end
